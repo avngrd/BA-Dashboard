5da8e7f3ac763510fc712095a4a73cd1616efb50
Vadim <vadimlazuk33@gmail.com>
2022-11-24 01:13:20 +0300

    footer stylize

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,13 @@
 const linkMainPage = document.querySelector('.footer-main-page');
 const mainPage = document.querySelector('.header__content');
 const linkSecureTitle = document.querySelector('.title-secure');
+const popUp = document.querySelector('.popup');
+const popUpClose = document.querySelector('.popup_close');
 
+let closePopUp = ()=>{
+  popUp.classList.add('.visually-hidden');
+}
+popUpClose.addEventListener('click', closePopUp);
 
 let scrollToPage = () => {
   mainPage.scrollIntoView({behavior:"smooth"});
