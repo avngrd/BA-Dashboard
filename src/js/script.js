536e7a0d124993867a4c237f83fb5c744cb4e64d
Vadim <vadimlazuk33@gmail.com>
2022-11-23 15:07:13 +0300

    footer stylize

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,11 @@
 const linkMainPage = document.querySelector('.footer-main-page');
 const mainPage = document.querySelector('.header__content');
+const linkSecureTitle = document.querySelector('.title-secure');
+
 
 let scrollToPage = () => {
   mainPage.scrollIntoView({behavior:"smooth"});
 };
+
 linkMainPage.addEventListener("click", scrollToPage);
+linkSecureTitle.addEventListener("click", scrollToPage);
